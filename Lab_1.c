febf82d397d36096f8ed0452f7a6a614890b92f7
Vadim <kulikovskiiv@mail.ru>
2024-09-13 08:16:54 +0300

    2

--- Lab_1.c
+++ Lab_1.c
@@ -1,14 +1,23 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
+#define N 10
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	int mas[] = { -41, 2, 32, 4, 5 };
+	srand(time(NULL));
+	int mas[N];
+	for (int i = 0; i < 10; ++i) {
+		mas[i] = rand() % 200 - 100;
+		printf("%d ", mas[i]);
+	}
+	printf("\n");
 	int min = mas[0], max = mas[0];
-	for (int i = 0; i < sizeof(mas) / sizeof(int); ++i) {
+	for (int i = 0; i < N; ++i) {
 		max = max > mas[i] ? max : mas[i];
 		min = min > mas[i] ? mas[i] : min;
 	}
-	printf("Разница между максимальным и минимальным элементами массива: %d", max - min);
+	printf("Разница между максимальным (%d) и минимальным (%d) элементами массива: %d", max, min, max - min);
+	return 0;
 }
