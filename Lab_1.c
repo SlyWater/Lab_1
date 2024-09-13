9e1a3f7b14363bbad71f13e5ee48b2f605c321ac
Vadim <kulikovskiiv@mail.ru>
2024-09-13 08:10:24 +0300

    Добавьте файлы проекта.

---
+++ Lab_1.c
@@ -0,0 +1,14 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int mas[] = { -41, 2, 32, 4, 5 };
+	int min = mas[0], max = mas[0];
+	for (int i = 0; i < sizeof(mas) / sizeof(int); ++i) {
+		max = max > mas[i] ? max : mas[i];
+		min = min > mas[i] ? mas[i] : min;
+	}
+	printf("Разница между максимальным и минимальным элементами массива: %d", max - min);
+}
